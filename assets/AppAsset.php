6eff7362669dfd13cdf1e87ff02d11a50c18001e
Piotr Soliński <solinski.mfs@gmail.com>
2019-07-08 23:10:51 +0200

    Clean project

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -1,9 +1,4 @@
-<?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
+<?
 
 namespace app\assets;
 
@@ -11,9 +6,6 @@ use yii\web\AssetBundle;
 
 /**
  * Main application asset bundle.
- *
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
  */
 class AppAsset extends AssetBundle
 {
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,4 +1,4 @@
-<?php
+<?
 
 /* @var $this \yii\web\View */
 /* @var $content string */
@@ -12,22 +12,22 @@ use app\assets\AppAsset;
 
 AppAsset::register($this);
 ?>
-<?php $this->beginPage() ?>
+<? $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?php $this->registerCsrfMetaTags() ?>
+    <? $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+    <? $this->head() ?>
 </head>
 <body>
-<?php $this->beginBody() ?>
+<? $this->beginBody() ?>
 
 <div class="wrap">
-    <?php
+    <?
     NavBar::begin([
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
@@ -59,10 +59,6 @@ AppAsset::register($this);
     ?>
 
     <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
         <?= $content ?>
     </div>
 </div>
@@ -75,7 +71,7 @@ AppAsset::register($this);
     </div>
 </footer>
 
-<?php $this->endBody() ?>
+<? $this->endBody() ?>
 </body>
 </html>
-<?php $this->endPage() ?>
+<? $this->endPage() ?>
